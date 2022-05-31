3b9239dd9160d9b0776439863bd7213ab1517ef5
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2022-05-31 23:38:42 +0300

    Fix malloc error.

--- phella/temp_env.c
+++ phella/temp_env.c
@@ -2,7 +2,8 @@
 
 void	temp_sort_env(t_env *e, char *line)
 {
-	int	i;
+	int		i;
+	char	**ptr;
 
 	i = -1;
 	e->temp_sort_env2 = malloc(sizeof(char *) * e->count_sort_var + 1);
@@ -13,12 +14,17 @@ void	temp_sort_env(t_env *e, char *line)
 	e->temp_sort_env2[e->count_sort_var] = ft_strdup(line + 7);
 	e->temp_sort_env2[e->count_sort_var + 1] = NULL;
 	i = -1;
+	ptr = e->sort_env2;
+	while (e->sort_env2[++i])
+		free(e->sort_env2[i]);
+	free(e->sort_env2);
 	e->sort_env2 = e->temp_sort_env2;
 }
 
 void	temp_env(t_env *e, char *line)
 {
-	int	i;
+	int		i;
+	char	**ptr;
 
 	i = -1;
 	e->temp_env2 = malloc(sizeof(char *) * e->count_var + 1);
@@ -29,5 +35,9 @@ void	temp_env(t_env *e, char *line)
 	e->temp_env2[e->count_var] = ft_strdup(line + 7);
 	e->temp_env2[e->count_var + 1] = NULL;
 	i = -1;
+	ptr = e->sort_env2;
+	while (e->env2[++i])
+		free(e->env2[i]);
+	free(e->env2);
 	e->env2 = e->temp_env2;
 }
